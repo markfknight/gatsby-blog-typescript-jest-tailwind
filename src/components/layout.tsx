5fd445b5fcb8b0f0a8dd31eb0f7dd2aefa53961c
markfknight <markfknight@gmail.com>
2020-10-14 18:03:23 +0100

    feat: add layout structure and header link

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,49 @@
-import React from 'react';
+import { Link, PageRendererProps } from 'gatsby';
+import React, { PropsWithChildren } from 'react';
+type LayoutProps = PageRendererProps & { title: string };
 
-const Layout: React.FC = ({ children }) => <div>{children}</div>;
+const Layout: React.FC<PropsWithChildren<LayoutProps>> = (props) => {
+  const { location, title, children } = props;
+
+  const header =
+    location.pathname === `${__PATH_PREFIX__}/` ? (
+      <h1>
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+            textDecoration: `none`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h1>
+    ) : (
+      <h3>
+        <Link
+          style={{
+            boxShadow: `none`,
+            color: `inherit`,
+            textDecoration: `none`,
+          }}
+          to={`/`}
+        >
+          {title}
+        </Link>
+      </h3>
+    );
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer>
+    </div>
+  );
+};
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,36 @@
+import { graphql, PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
 import { SEO } from '../components/seo';
 
-const Home: React.FC = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div>Hello world!</div>
-  </Layout>
-);
+type Data = {
+  site: {
+    siteMetadata: {
+      title: string;
+    };
+  };
+};
+
+const Home: React.FC<PageProps<Data>> = (props) => {
+  const { data } = props;
+  const title = data.site.siteMetadata.title;
+
+  return (
+    <Layout location={props.location} title={title}>
+      <SEO title="Home" />
+      <div>Hello world!</div>
+    </Layout>
+  );
+};
 
 export default Home;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`;
