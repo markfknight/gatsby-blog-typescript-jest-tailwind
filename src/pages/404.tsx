15be8e4e71bc9ac3fcf592b5cb4904895c40a58a
markfknight <markfknight@gmail.com>
2020-12-05 15:23:02 +0000

    fix: correct 404 SEO import

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import { graphql, PageProps } from 'gatsby';
 import React from 'react';
 import Layout from '../components/layout';
-import SEO from '../components/seo';
+import { SEO } from '../components/seo';
 
 type Data = {
   site: {
