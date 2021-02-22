c6cc700b7ee6ebc1b2eb395beb1499327b590603
markfknight <markfknight@gmail.com>
2021-02-22 23:37:03 +0000

    test(page): add snapshot testing for pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import { SEO } from '../components/seo';
 
-type Data = {
+export type Data = {
   site: {
     siteMetadata: {
       title: string;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/layout';
 import { PostList } from '../components/post-list';
 import { SEO } from '../components/seo';
 
-type Data = {
+export type Data = {
   site: {
     siteMetadata: {
       title: string;
