b4f6358feb4658e01f6396e31776c155fbd91657
markfknight <markfknight@gmail.com>
2021-03-01 19:50:38 +0000

    feat: add github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,8 @@
 
 module.exports = {
   /* Your site config here */
+  // [GitHub Pages](https://www.gatsbyjs.com/docs/how-to/previews-deploys-hosting/how-gatsby-works-with-github-pages/)
+  pathPrefix: `/gatsby-blog-typescript-jest-tailwind`,
   siteMetadata: {
     title: `Gatsby Blog Starter`,
     description: `Gatsby Blog TypeScript with esLint, prettier, Jest and Tailwind`,
